c50b4d1b9ab870ddfc632f0e666e50ee7ec97124
lizaiyang <lizaiyangs@outlook.com>
2019-11-06 09:31:38 +0800

    添加注释

--- init/app/service/price.js
+++ init/app/service/price.js
@@ -4,6 +4,7 @@ class PriceService extends Service {
   async allPrice(ctx) {
     // 业务层
     for (let index = 0; index < 50; index++) {
+      // 这个执行多少次就会启动多少个进程
       await this.app.messenger.sendToAgent('demo-worker', { index })
     }
   }
