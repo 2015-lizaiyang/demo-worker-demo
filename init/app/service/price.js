2f8a22ecbb56d9f6d1a7183387f6ed1950c81d18
lizaiyang <lizaiyangs@outlook.com>
2019-11-06 10:17:02 +0800

    添加注释

--- init/app/service/price.js
+++ init/app/service/price.js
@@ -3,7 +3,7 @@ const Service = require('egg').Service;
 class PriceService extends Service {
   async allPrice(ctx) {
     // 业务层
-    for (let index = 0; index < 50; index++) {
+    for (let index = 0; index < 1000; index++) {
       // 这个执行多少次就会启动多少个进程
       await this.app.messenger.sendToAgent('demo-worker', { index })
     }
